2619e83c40ea9354a14e8cb5526450146b693a97
xiebian <xiebianwhu@gmail.com>
2021-09-24 18:07:10 +0800

    add backslash at the end of job detail url

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('joblist/', views.job_list, name='joblist'),
-    path('job/<int:job_id>', views.job_detail, name='job_detail')
+    path('job/<int:job_id>/', views.job_detail, name='job_detail')
 ]
